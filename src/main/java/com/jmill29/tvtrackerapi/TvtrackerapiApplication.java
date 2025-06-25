11df5aee368aa2878f748e0fef8d1084f5347903
Jacob Miller <jacoblmiller.jlm@gmail.com>
2025-06-25 15:22:16 -0400

    Initial commit

---
+++ src/main/java/com/jmill29/tvtrackerapi/TvtrackerapiApplication.java
@@ -0,0 +1,13 @@
+package com.jmill29.tvtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvtrackerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvtrackerapiApplication.class, args);
+	}
+
+}
