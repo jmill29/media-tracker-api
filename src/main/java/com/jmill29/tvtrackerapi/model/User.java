8313b91f9a8618209afc8887bb612424c603fece
Jacob Miller <jacoblmiller.jlm@gmail.com>
2025-07-01 16:31:32 -0400

    refactor: reorder User constructor params to match field order docs: add Javadoc to User model

--- src/main/java/com/jmill29/tvtrackerapi/model/User.java
+++ src/main/java/com/jmill29/tvtrackerapi/model/User.java
@@ -2,25 +2,46 @@ package com.jmill29.tvtrackerapi.model;
 
 import java.time.LocalDateTime;
 
+/**
+ * Model representing a user in the system.
+ */
 public class User {
 
+    /** The unique ID of the user */
     private int userId;
+    /** The full name of the user */
     private String name;
+    /** The username of the user */
     private String username;
+    /** The user's password (should be stored as a hash) */
     private String password;
+    /** The email address of the user */
     private String email;
+    /** The date and time the user was created in the system */
     private LocalDateTime createdAt;
 
+    /**
+     * Default constructor.
+     */
     public User() {
     }
 
-    public User(LocalDateTime createdAt, String email, String name, String password, int userId, String username) {
-        this.createdAt = createdAt;
-        this.email = email;
-        this.name = name;
-        this.password = password;
+    /**
+     * Constructs a User with all fields.
+     * @param userId the unique ID of the user
+     * @param name the user's full name
+     * @param username the user's username
+     * @param password the user's password (should be hashed)
+     * @param email the user's email address
+     * @param createdAt the date and time the user was created
+     */
+    public User(int userId, String name, String username, String password, String email, LocalDateTime createdAt) {
         this.userId = userId;
+        this.name = name;
         this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
     }
 
     public int getUserId() {
@@ -71,6 +92,11 @@ public class User {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Returns a string representation of the User object.
+     * Useful for logging and debugging purposes.
+     * @return a string representation of the user
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
