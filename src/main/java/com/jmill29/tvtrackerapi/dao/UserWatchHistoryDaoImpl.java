714e29f7ac8d7128f68fb29a5789f9b9cf26d7c5
Jacob Miller <jacoblmiller.jlm@gmail.com>
2025-07-02 16:48:46 -0400

    Implement getWatchHistoryByUsername in UserWatchHistoryDaoImpl with support for full or filtered results

--- src/main/java/com/jmill29/tvtrackerapi/dao/UserWatchHistoryDaoImpl.java
+++ src/main/java/com/jmill29/tvtrackerapi/dao/UserWatchHistoryDaoImpl.java
@@ -105,6 +105,36 @@ public class UserWatchHistoryDaoImpl implements UserWatchHistoryDao {
             return rs.next();
         }
     }
+
+    @Override
+    public List<UserWatchHistoryDto> getWatchHistoryByUsername(String username, boolean getAll) throws SQLException {
+        try (Connection conn = dataSource.getConnection()) {
+            String query;
+            if (!getAll) {
+                // Only shows the user has interacted with (INNER JOIN)
+                query = "SELECT uwh.show_id, s.show_name AS show_name, s.description, s.image_url, uwh.status " +
+                        "FROM user_watch_history uwh " +
+                        "JOIN tv_shows s ON uwh.show_id = s.show_id " +
+                        "JOIN users u ON uwh.user_id = u.user_id AND u.username = ?";
+            } else {
+                // All shows, with user's status if present (LEFT JOIN)
+                query = "SELECT s.show_id, s.show_name AS show_name, s.description, s.image_url, uwh.status " +
+                        "FROM tv_shows s " +
+                        "LEFT JOIN user_watch_history uwh ON s.show_id = uwh.show_id AND uwh.user_id = " +
+                        "(SELECT user_id FROM users WHERE username = ?)";
+            }
+            PreparedStatement pStmt = conn.prepareStatement(query);
+            pStmt.setString(1, username);
+            ResultSet rs = pStmt.executeQuery();
+            List<UserWatchHistoryDto> watchHistory = new ArrayList<>();
+            while (rs.next()) {
+                // Map each row to a DTO, handling null status
+                watchHistory.add(mapUserWatchHistoryDto(rs));
+            }
+            return watchHistory;
+        }
+    }
+
     
     private UserWatchHistoryDto mapUserWatchHistoryDto(ResultSet rs) throws SQLException {
         // Map a ResultSet row to a UserWatchHistoryDto, defaulting status if null
