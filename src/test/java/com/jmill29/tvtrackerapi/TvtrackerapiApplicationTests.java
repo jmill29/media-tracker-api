4c80a8b472a18c687b14fb998480a69886d05935
Jacob Miller <jacoblmiller.jlm@gmail.com>
2025-07-04 20:53:18 -0400

    Add display names to application context test
    - Annotated TvtrackerapiApplicationTests with @DisplayName for improved test readability
    - Labeled contextLoads() to clarify intent during test runs

--- src/test/java/com/jmill29/tvtrackerapi/TvtrackerapiApplicationTests.java
+++ src/test/java/com/jmill29/tvtrackerapi/TvtrackerapiApplicationTests.java
@@ -1,12 +1,15 @@
 package com.jmill29.tvtrackerapi;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayName("Tvtrackerapi Application Tests")
 class TvtrackerapiApplicationTests {
 
 	@Test
+	@DisplayName("Spring context loads without error")
 	void contextLoads() {
 	}
 
